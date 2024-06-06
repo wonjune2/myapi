1f92cf305511fc295744ee4354a7f59566ea9707
wonjune2 <zzzdnjswns@naver.com>
2024-06-07 07:10:44 +0900

    0607

---
+++ frontend/src/routes/Detail.svelte
@@ -0,0 +1,39 @@
+<script>
+    import fastapi from "../lib/api";
+
+    export let params = {}
+    let question_id = params.question_id
+    let question = {}
+    let content = ""
+
+   function get_question() {
+        fastapi("get", "/api/question/detail/" + question_id, {}, (json) => {
+            question = json
+        })
+    }
+
+    get_question()
+
+    function post_answer(event) {
+        event.preventDefault()
+        let url = "/api/answer/create/" + question_id
+        let params = {
+            content: content
+        }
+        fastapi('post', url, params, 
+            (json) => {
+                content = ''
+                get_question()
+            }
+        )
+    }
+</script>
+
+<h1>{question.subject}</h1>
+<div>
+    {question.content}
+</div>
+<form method="post">
+    <textarea rows="15" bind:value={content}></textarea>
+    <input type="submit" value="답변등록" on:click="{post_answer}">
+</form>
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from domain.answer import answer_router
+from domain.question import question_router
 
 app = FastAPI()
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요 파이보"}
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(question_router.router)
+app.include_router(answer_router.router)
